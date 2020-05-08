57be22db88b31aed6f735617e0a3b1f6258a65a0
sergeponomaryov <ponomaryovserge@gmail.com>
2020-05-08 20:26:18 +0200

    start using webpack and npm

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+   module: {
+     rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+          ],
+        },
+     ],
+   },
+   plugins: [
+     // make sure to include the plugin for the magic
+     new VueLoaderPlugin()
+   ]
+};
